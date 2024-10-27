72180a5885adb39c48b4bb53f1823b9e799bd094
Jakub Mesticky <mesticky.jakub7@gmail.com>
2024-10-27 03:05:23 +0100

    adding pages

---
+++ src/app/movieapp/pages/movies/movies.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { GridComponent } from '../../components/grid/grid.component';
+import { Observable, take, tap } from 'rxjs';
+import { Movie } from '../../store/movies/movie.model';
+import { selectAllMovies,  selectMoviesError } from '../../store/movies/movie.selector';
+import { select, Store } from '@ngrx/store';
+import * as MovieActions from '../../store/movies/movie.action';
+
+@Component({
+  selector: 'app-movies',
+  standalone: true,
+  imports: [GridComponent],
+  templateUrl: './movies.component.html',
+  styleUrl: './movies.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MoviesComponent {
+  movies$!: Observable<Movie[]| []>;
+  loading$!: Observable<boolean>;
+  error$!: Observable<string | null>;
+  currentPage$!: Observable<number>;
+
+  @ViewChild('scrollContainer', { static: true }) scrollContainer!: ElementRef;
+
+  constructor(private store: Store) {
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(MovieActions.getMovie({page: 1}));
+    this.movies$ = this.store.pipe(select(selectAllMovies));
+    this.error$ = this.store.pipe(select(selectMoviesError));
+  //  this.currentPage$ = this.store.pipe(select(selectCurrentPage));
+  }
+
+  handleScroll(event: {collectionName: string, page: number}){
+    this.store.dispatch(MovieActions.getMovie({page: event.page}));
+  }
+
+
+}
---
+++ src/app/movieapp/pages/tv-show/tv-show.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2, ChangeDetectionStrategy } from '@angular/core';
+import { GridComponent } from '../../components/grid/grid.component';
+import { Observable, take, tap } from 'rxjs';
+import { Movie } from '../../store/movies/movie.model';
+import { Store } from '@ngrx/store';
+import * as TVShowAction from '../../store/tv-shows/tv-show.action';
+import { selectAllTVShows, selectTVShowsError } from '../../store/tv-shows/tv-show.selector';
+
+@Component({
+  selector: 'app-tv-show',
+  standalone: true,
+  imports: [GridComponent],
+  templateUrl: './tv-show.component.html',
+  styleUrl: './tv-show.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TvShowComponent{
+  tvshow$: Observable<Movie[]>;
+  error$: Observable<string | null>;
+  currentPage: number = 1;
+
+  constructor(private store: Store,private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(TVShowAction.getTVShow({page: 1}));
+    this.tvshow$ = this.store.select(selectAllTVShows);
+    this.error$ = this.store.select(selectTVShowsError);
+  }
+
+  handleScroll(event: {collectionName: string, page: number}){
+    this.store.dispatch(TVShowAction.getTVShow({page: event.page}));
+  }
+}
