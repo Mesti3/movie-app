2f8ff340403a28008b741266ce649bd25342c664
Jakub Mesticky <mesticky.jakub7@gmail.com>
2024-10-27 03:02:47 +0100

    setting up movie app

---
+++ src/app/movieapp/movieapp.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MovieRoutingModule } from './movieapp-routing.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { GridComponent } from './components/grid/grid.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TvShowComponent } from './pages/tv-show/tv-show.component';
+import { AboutComponent} from './pages/about/about.component';
+import { ListComponent} from './components/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MovieRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FooterComponent,
+    GridComponent,
+    HomeComponent,
+    MoviesComponent,
+    HeaderComponent,
+    TvShowComponent,
+    AboutComponent,
+    ListComponent,
+    ListItemComponent
+  ],
+})
+export class MovieAppModule {}
