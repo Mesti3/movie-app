7a6e182faaf3857ff3deb78a4e2c295e94255a44
Jakub Mesticky <mesticky.jakub7@gmail.com>
2024-10-27 03:01:25 +0100

    initializating project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './movieapp/pages/home/home.component';
+import {TodoHomeComponent} from './todo/pages/todo-home/todo-home.component'; 
+import { MoviesComponent } from './movieapp/pages/movies/movies.component';
+import { TvShowComponent } from './movieapp/pages/tv-show/tv-show.component';
+import { AboutComponent } from './movieapp/pages/about/about.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+      path: 'home',
+      component: HomeComponent
+    },
+      {
+        path: 'todo',
+        component: TodoHomeComponent
+      },
+      {
+        path: 'movies',
+        component: MoviesComponent
+      },
+      {
+        path: 'tv-shows',
+        component: TvShowComponent,
+      },
+      {
+        path: 'about',
+       component: AboutComponent
+      },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,49 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { MovieEffects } from './app/movieapp/store/movies/movie.effect';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { movieReducer } from './app/movieapp/store/movies/movie.reducer';
+import { StoreModule } from '@ngrx/store';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MovieAppModule } from './app/movieapp/movieapp.module';
+import {
+  withInterceptorsFromDi,
+  provideHttpClient,
+} from '@angular/common/http';
+import { AppRoutingModule } from './app/app.routes';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { TodoEffect } from './app/todo/store/todo.effects';
+import { TVShowReducer } from './app/movieapp/store/tv-shows/tv-show.reducer';
+import { SearchReducer } from './app/movieapp/store/search/search.reducer';
+import { TodoReducer } from './app/todo/store/todo.reducers';
+import { SearchEffects } from './app/movieapp/store/search/search.effect';
+import { TVShowEffects } from './app/movieapp/store/tv-shows/tv-show.effect';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
+      AppRoutingModule,
+      MovieAppModule,
+      StoreModule.forRoot({ movie: movieReducer, tvshow: TVShowReducer , search: SearchReducer, todos: TodoReducer}),
+      StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        logOnly: environment.production,
+        connectInZone: true,
+      }),
+      EffectsModule.forRoot([MovieEffects, TodoEffect, SearchEffects, TVShowEffects])
+    ),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(),
+  ],
+}).catch((err) => console.error(err));
